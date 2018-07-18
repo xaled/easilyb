698123e103615da6926cc55083d5721534727122
xaled <kh.grandi@gmail.com>
2018-07-18 15:53:53 +0100

    adding kwargs and result_queue to threaded function

--- easilyb/threads/__init__.py
+++ easilyb/threads/__init__.py
@@ -1,8 +1,22 @@
 import threading
+import queue
 
 
-def threaded(func, args, daemon=False):
-    t = threading.Thread(target=func, args=args)
+def threaded(func, args=None, kwargs=None, daemon=False):
+    args = args or ()
+    kwargs = kwargs or {}
+
+    def wrapped(q, *a, **kw):
+        ret = func(*a, **kw)
+        q.put(ret)
+
+    q = queue.Queue()
+    if isinstance(args, tuple):
+        args = (q,)+args
+    else:
+        args = [q,] + args
+    t = threading.Thread(target=wrapped, args=(q,)+args, kwargs=kwargs)
     t.setDaemon(daemon)
     t.start()
+    t.result_queue = q
     return t
