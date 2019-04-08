2dd1c0353c36df6685396d78efa1bd3a0be863db
xaled <kh.grandi@gmail.com>
2019-04-08 13:25:54 +0100

    build and deploy script

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
 from distutils.core import setup
-
 import os
+
+VERSION = '0.17.0'  # major.minor.fix
+                    # MAJOR incompatible API changes, MINOR add backwards-compatible functionality, FIX bug fixes
 
 
 def package_files(directory):
@@ -13,22 +14,24 @@ def package_files(directory):
                 paths.append(os.path.relpath(os.path.join(path, filename), directory))
     return paths
 
-extra_files = package_files('easilyb/')
 
-#print extra_files
+if __name__ == "__main__":
+    extra_files = package_files('easilyb/')
 
-setup(
-      name='easilyb',
-      version='0.17.0', # major.minor.fix: MAJOR incompatible API changes, MINOR add backwards-compatible functionality, FIX bug fixes
-      description='Frequently used functions library for Python3 By Khalid Grandi (github.com/xaled).',
-      long_description='Frequently used functions library for Python3 By Khalid Grandi (github.com/xaled).',
-      long_description_content_type='text/x-rst',
-      keywords='library utils common',
-      author='Khalid Grandi',
-      author_email='kh.grandi@gmail.com',
-      url='https://github.com/xaled/easilyb/',
-      install_requires=['requests', 'pycrypto', 'pyaml', 'lxml'],
-      python_requires='>=3',
-      packages=['easilyb'],
-      package_data={'': extra_files},
-     )
+    # print extra_files
+
+    setup(
+        name='easilyb',
+        version=VERSION,
+        description='Frequently used functions library for Python3 By Khalid Grandi (github.com/xaled).',
+        long_description='Frequently used functions library for Python3 By Khalid Grandi (github.com/xaled).',
+        long_description_content_type='text/x-rst',
+        keywords='library utils common',
+        author='Khalid Grandi',
+        author_email='kh.grandi@gmail.com',
+        url='https://github.com/xaled/easilyb/',
+        install_requires=['requests', 'pycrypto', 'pyaml', 'lxml'],
+        python_requires='>=3',
+        packages=['easilyb'],
+        package_data={'': extra_files},
+    )
